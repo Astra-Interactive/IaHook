b791c40902f8986fbcef54139488f00bf0a3f2d3
Roman Makeev <makeevrserg@gmail.com>
2023-07-19 22:43:50 +0300

    #5 - disable bypass armor for specific items - fixed stackoverflow

--- plugin/src/main/kotlin/ru/astrainteractive/iahook/events/events/MultipleEventsDSL.kt
+++ plugin/src/main/kotlin/ru/astrainteractive/iahook/events/events/MultipleEventsDSL.kt
@@ -6,21 +6,31 @@ import org.bukkit.entity.Player
 import org.bukkit.event.entity.EntityDamageByEntityEvent
 import ru.astrainteractive.astralibs.events.DSLEvent
 import ru.astrainteractive.iahook.events.di.EventModule
+import java.util.UUID
 
 /**
  * This is a most convenient way to use bukkit events in kotlin
  */
 class MultipleEventsDSL(module: EventModule) : EventModule by module {
 
+    private val customDamages = HashSet<UUID>()
+
     val entityDamageByEntityEvent = DSLEvent<EntityDamageByEntityEvent>(eventListener, plugin) {
         val playerDamager = it.damager as? Player ?: return@DSLEvent
         val damageItem = playerDamager.inventory.itemInMainHand ?: return@DSLEvent
         val customDamageItem = CustomStack.byItemStack(damageItem) ?: return@DSLEvent
-        if (it.damage != 0.0 || it.finalDamage != 0.0) return@DSLEvent
         configuration.damageEntitiesItemIds.firstOrNull { customDamageItem.id == it } ?: return@DSLEvent
         val entity = it.entity as? LivingEntity ?: return@DSLEvent
+        if (customDamages.contains(it.entity.uniqueId)) {
+            customDamages.remove(it.entity.uniqueId)
+            return@DSLEvent
+        }
+        customDamages.add(it.entity.uniqueId)
         val lastDamage = entity.lastDamage
         entity.lastDamage = 0.0
+        it.damage = 0.0
+        it.isCancelled = true
+
         entity.damage(lastDamage, playerDamager)
     }
 }
