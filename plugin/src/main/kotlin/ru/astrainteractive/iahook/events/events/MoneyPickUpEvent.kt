237a6be5ea05d8f057159e10f09d155abddef0cb
Roman Makeev <makeevrserg@gmail.com>
2023-09-09 13:23:18 +0300

    fix: money pick up by hopper

--- plugin/src/main/kotlin/ru/astrainteractive/iahook/events/events/MoneyPickUpEvent.kt
+++ plugin/src/main/kotlin/ru/astrainteractive/iahook/events/events/MoneyPickUpEvent.kt
@@ -6,6 +6,10 @@ import org.bukkit.event.entity.EntityPickupItemEvent
 import ru.astrainteractive.astralibs.events.DSLEvent
 import ru.astrainteractive.iahook.events.di.EventModule
 import kotlin.random.Random
+import org.bukkit.event.inventory.InventoryMoveItemEvent
+import org.bukkit.event.inventory.InventoryPickupItemEvent
+import org.bukkit.event.inventory.InventoryType
+import org.bukkit.inventory.ItemStack
 
 class MoneyPickUpEvent(module: EventModule) : EventModule by module {
     val playerPickUpEvent = DSLEvent<EntityPickupItemEvent>(eventListener, plugin) { e ->
@@ -32,5 +36,21 @@ class MoneyPickUpEvent(module: EventModule) : EventModule by module {
         if (configuration.logging.moneyPickUp) {
             logger.info("MoneyPickUpEvent", "Player picked up $amount $id, received $money: $result")
         }
+    }
+
+    private fun isMoneyItem(itemStack: ItemStack): Boolean {
+        val amount = itemStack.amount
+        val id = CustomStack.byItemStack(itemStack)?.id ?: itemStack.type.name
+        configuration.moneyPickUp.items.entries.firstOrNull { it.value.item == id } ?: return false
+        return true
+    }
+
+    val inventoryMoveEvent = DSLEvent<InventoryMoveItemEvent>(eventListener, plugin) { e ->
+        if (!isMoneyItem(e.item)) return@DSLEvent
+        e.isCancelled = true
+    }
+    val inventoryPickupItemEvent = DSLEvent<InventoryPickupItemEvent>(eventListener, plugin) { e ->
+        if (!isMoneyItem(e.item.itemStack)) return@DSLEvent
+        e.isCancelled = true
     }
 }
